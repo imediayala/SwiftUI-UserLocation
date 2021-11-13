5b38614413380ad36b9cb54e52ace8d611fa155f
Daniel Ayala <imediayala@gmail.com>
2021-11-13 11:27:44 +0100

    Implemented location manager

---
+++ SwiftUI-UserLocation/MapView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  SwiftUI-UserLocation
+//
+//  Created by Daniel Ayala on 13/11/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @StateObject private var viewModel =  MapViewModel()
+    
+    var body: some View {
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .accentColor(Color(.systemPink))
+            .onAppear {
+                viewModel.checkIfLocationManagerIsEnabled()
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.416729, longitude: -3.703339), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
+    func checkIfLocationManagerIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        } else {
+            print("Show alert asking to turn on location services")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted")
+        case .denied:
+            print("You have denied your location permission")
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+}
